ceeb66adbca8df6b2262e9241a7a1c48f6a1e4cd
naluwan <street.baller06@gmail.com>
2023-10-30 13:51:42 +0800

    feat: add backgroundImage, fontFamily and custom width, height for iframe

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,6 +40,20 @@ module.exports = {
       borderRadius: {
         '5xl': '40px',
       },
+      backgroundImage: {
+        'yt-bg-img':
+          // eslint-disable-next-line quotes
+          'url(\'data:image/svg+xml,%3Csvg width="6" height="6" viewBox="0 0 6 6" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="%23000000" fill-opacity="0.4" fill-rule="evenodd"%3E%3Cpath d="M5 0h1L0 6V5zM6 5v1H5z"/%3E%3C/g%3E%3C/svg%3E\')',
+      },
+      width: {
+        'video-width': 'var(--video-width)',
+      },
+      height: {
+        'video-height': 'var(--video-height)',
+      },
+      fontFamily: {
+        bebas_neue: ['Bebas Neue', 'sans-serif'],
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
