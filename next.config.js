0473456859297a2b70d67a9a047b0fa14f170967
naluwan <street.baller06@gmail.com>
2023-10-30 19:27:25 +0800

    feat: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== 'production';
 const nextConfig = {
   images: {
     domains: ['utfs.io'],
   },
+  assetPrefix: !debug ? 'https://focusspace-fa98b8543b50.herokuapp.com/' : '',
 };
 
 module.exports = nextConfig;
