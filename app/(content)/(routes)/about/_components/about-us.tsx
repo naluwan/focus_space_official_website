9d3840429677d79cc7d0c07f7b1469b8fca85763
naluwan <street.baller06@gmail.com>
2023-10-31 21:51:28 +0800

    feat: add about-us component

---
+++ app/(content)/(routes)/about/_components/about-us.tsx
@@ -0,0 +1,23 @@
+import LeftContent from './left-content';
+import RightContent from './right-content';
+import gymPhoto1 from '@/public/aboutGym.jpg';
+import gymPhoto2 from '@/public/aboutGym2.jpg';
+
+const AboutUs = () => {
+  return (
+    <>
+      <RightContent
+        img={gymPhoto1}
+        title='關於我們'
+        content='FOCUS_SPACE 專心練運動空間開幕於2023年4月30日，旨在推廣大眾強身、健體之觀念，在運動的同時給予專業的建議和正確的觀念與姿勢，藉此保護所有運動員的安全與提升運動效果。'
+      />
+      <LeftContent
+        img={gymPhoto2}
+        title='健身房簡介'
+        content='FOCUS_SPACE 專心練運動空間擁有300坪大空間重量訓練區、齊全的有氧設備並附有淋浴區及休息區，專屬的APP條碼入場，不綁約、單次數場、分鐘入場，讓你更靈活的安排運動時間。'
+      />
+    </>
+  );
+};
+
+export default AboutUs;
