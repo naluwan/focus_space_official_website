201f813b0c0c0ff8405e8d81743b5c21e1077e88
naluwan <street.baller06@gmail.com>
2023-10-31 21:52:28 +0800

    feat: update About page

--- app/(content)/(routes)/about/page.tsx
+++ app/(content)/(routes)/about/page.tsx
@@ -1,5 +1,22 @@
+import AboutNavigateBar from './_components/about-navigatebar';
+import AboutUs from './_components/about-us';
+import AboutHero from './_components/about-hero';
+import AboutIntroduce from './_components/about-introduce';
+
+import heroImg1 from '@/public/aboutHero.jpeg';
+import heroImg2 from '@/public/aboutHero2.jpg';
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className='relative h-full bg-black text-white'>
+      {/* TODO:導覽列 */}
+      <AboutNavigateBar />
+      <AboutHero img={heroImg1} title='about us' />
+      <AboutUs />
+      <AboutHero img={heroImg2} title='founder' />
+      <AboutIntroduce />
+    </div>
+  );
 };
 
 export default AboutPage;
