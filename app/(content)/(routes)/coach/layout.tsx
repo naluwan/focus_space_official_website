57cfa886729377105b63891efde190f85d9a86f9
naluwan <street.baller06@gmail.com>
2023-11-13 15:27:50 +0800

    feat: update title template of metadata

--- app/(content)/(routes)/coach/layout.tsx
+++ app/(content)/(routes)/coach/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 import Page from './page';
 
 export const metadata: Metadata = {
-  title: '教練陣容',
+  title: '教練陣容 ',
   description:
     'Focus Space 專心練運動空間擁有數十位專業教練，除了專業證照，更有豐富的履歷、經驗，能給予所有的運動員最好的體驗。',
 };
