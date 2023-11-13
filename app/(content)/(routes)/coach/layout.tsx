a1fb262f6c52c79894f3c91b9946b00528ddd2c4
naluwan <street.baller06@gmail.com>
2023-11-13 15:43:16 +0800

    feat: update description of metadata for Coach page and About page

--- app/(content)/(routes)/coach/layout.tsx
+++ app/(content)/(routes)/coach/layout.tsx
@@ -4,7 +4,7 @@ import Page from './page';
 export const metadata: Metadata = {
   title: '教練陣容 ',
   description:
-    'Focus Space 專心練運動空間擁有數十位專業教練，除了專業證照，更有豐富的履歷、經驗，能給予所有的運動員最好的體驗。',
+    'Focus Space 專心練擁有數十位專業教練，除了專業證照，更有豐富的履歷、經驗，能給予所有的運動員在健身房最好的體驗並給予最安全的運動空間。',
 };
 
 export default Page;
