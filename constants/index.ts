3d36eb9fb918edf900044211f1065731aff2212d
naluwan <street.baller06@gmail.com>
2023-11-21 21:53:12 +0800

    Updates

---
+++ constants/index.ts
@@ -0,0 +1,44 @@
+import coach1 from '@/public/coach/1.jpg';
+import coach2 from '@/public/coach/2.jpg';
+import coach3 from '@/public/coach/3.jpg';
+import coach4 from '@/public/coach/4.jpg';
+import coach5 from '@/public/coach/5.jpg';
+import coach7 from '@/public/coach/6.jpg';
+import coach6 from '@/public/coach/7.jpg';
+import coach8 from '@/public/coach/8.jpg';
+import coach9 from '@/public/coach/9.jpg';
+import coach10 from '@/public/coach/10.jpg';
+import coach11 from '@/public/coach/11.jpg';
+import coach12 from '@/public/coach/12.jpg';
+import coach13 from '@/public/coach/13.jpg';
+import coach14 from '@/public/coach/14.jpg';
+import coach15 from '@/public/coach/15.jpg';
+import coach16 from '@/public/coach/16.jpg';
+import coach17 from '@/public/coach/17.jpg';
+
+export const NAV_LINKS = [
+  { href: '/news', key: 'news', label: '最新消息' },
+  { href: '/about', key: 'about', label: '關於我們' },
+  { href: '/class', key: 'class', label: '專心練課程' },
+  { href: '/coach', key: 'coach', label: '教練陣容' },
+];
+
+export const COACH_DATA = [
+  { img: coach1, key: 'coach1' },
+  { img: coach2, key: 'coach2' },
+  { img: coach3, key: 'coach3' },
+  { img: coach4, key: 'coach4' },
+  { img: coach5, key: 'coach5' },
+  { img: coach6, key: 'coach6' },
+  { img: coach7, key: 'coach7' },
+  { img: coach8, key: 'coach8' },
+  { img: coach9, key: 'coach9' },
+  { img: coach10, key: 'coach10' },
+  { img: coach11, key: 'coach11' },
+  { img: coach12, key: 'coach12' },
+  { img: coach13, key: 'coach13' },
+  { img: coach14, key: 'coach14' },
+  { img: coach15, key: 'coach15' },
+  { img: coach16, key: 'coach16' },
+  { img: coach17, key: 'coach17' },
+];
